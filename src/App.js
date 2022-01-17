f737ebed5848decda5e1d235715c783140ccc53d
ashma parveen <ashma.parveen@accenture.com>
2022-01-17 19:58:25 +0530

    my project22

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h2>My first Sticker Online Store.</h2>
+        <h2>My first Sticker Online Store.<br/> testtt </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
